68bf32130eae9fbd37d2437e0ff4ee6ad49f220c
Aderaldo <juniorcosta2611@gmail.com>
2026-03-23 20:28:55 -0300

    add evento de submit ao botton

--- src/js/investimentGoals.js
+++ src/js/investimentGoals.js
@@ -2,7 +2,7 @@ function convertToMonthlyReturnRate(yearReturnRate) {
   return yearReturnRate ** (1 / 12);
 }
 
-function generateReturnArray(
+export function generateReturnArray(
   startingAmount = 0,
   timeHorizon = 0,
   timePeriod = 'monthly',
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,40 @@
 import '../style/style.css';
+import { generateReturnArray } from './investimentGoals.js';
+
+const calculateButton = document.getElementById('calculateResoults');
+
+function renderProgression(event) {
+  event.preventDefault();
+  const startingAmount = Number(
+    document.getElementById('initialInvestment').value,
+  );
+
+  const additionalContributions = Number(
+    document.getElementById('additionalContributions').value,
+  );
+
+  const investmentTime = Number(
+    document.getElementById('investmentTime').value,
+  );
+
+  const investmentTimePeriod = document.getElementById(
+    'investmentTimePeriod',
+  ).value;
+
+  const returnRate = Number(document.getElementById('returnRate').value);
+
+  const evaluationPeriod = document.getElementById('evaluationPeriod').value;
+
+  const returnArray = generateReturnArray(
+    startingAmount,
+    investmentTime,
+    investmentTimePeriod,
+    additionalContributions,
+    returnRate,
+    evaluationPeriod,
+  );
+
+  console.log(returnArray);
+}
+
+calculateButton.addEventListener('submit', renderProgression);
